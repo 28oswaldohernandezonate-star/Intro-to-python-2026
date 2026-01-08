be7fbf829972b942693154e3d568c39f7802740d
28oswaldohernandezonate-star <28.oswaldo.hernandezonate@carolinaday.org>
2026-01-08 14:14:56 +0000

    Class Work

--- Tuesday.py
+++ Tuesday.py
@@ -1,1 +1,11 @@
-print("happy New Year")
+int = 6 
+float = 3.2
+string1 = ("One")
+string2 = ("Two")
+
+print(str(int) + "," + str(float) + "," + str(string1) + "," + str(string2) + ".") 
+
+int = input ("what should int be replaced with? >")
+string1 = input ("what should string1 be replaced with? >")
+
+print(str(int) + "," + str(float) + "," + str(string1) + "," + str(string2) + ".") 
