13390a41274b478bb17a759465babfc25c451e3f
28oswaldohernandezonate-star <28.oswaldo.hernandezonate@carolinaday.org>
2026-02-10 20:29:34 +0000

    Day two

--- Project.py
+++ Project.py
@@ -1,7 +1,7 @@
 import os, time #os allows to command line time allows to create slowtext 
 
 # creates the letter by letter visueal while text is displayed
-def slowText(text, delay=0.01):
+def slowText(text, delay=0.07):
     for char in text:
         print(char, end='', flush=True)
         time.sleep(delay)
@@ -16,15 +16,17 @@ def start():
     playerName = input()
     slowText("Hello, {}! You find yourself in the living room of a mysterious house.".format(playerName))
     slowText("From here, you can go to the kitchen, the bedroom, or the garden.")
-    time.sleep(3)
+    time.sleep(2)
     livingRoom()
 
 
 # livingroom gives you option to switch room by putting in input
 
 def livingRoom(): 
+    global inv
     os.system('cls' if os.name == 'nt' else 'clear')
-    slowText("You are in the living room. There are doors to the kitchen, bedroom, and garden.")
+    print("backpack: {}".format(backpack))
+    slowText("You are in the living room. There are doors to the kitchen, bedroom, and garden. You can also search for items")
     slowText("What would you like to do?")
     choice = input().strip().lower()
     if choice == "kitchen":
@@ -33,55 +35,84 @@ def livingRoom():
         bedroom()
     elif choice == "garden":
         garden()
+    elif choice == "search for items":
+        slowText("You searched the room and found a ")
     else:
         print("Invalid choice. Please try again.")
         time.sleep(2)
         livingRoom()
 
 
-def kitchen(): 
+def bedroom(): 
+    global inv
     os.system('cls' if os.name == 'nt' else 'clear')
-    slowText("You are in the kitchen. There is a door to the living room.")
+    print("backpack: {}".format(backpack))
+    slowText("You are in the bedroom. There is a door to the living room. You can also search for items")
     slowText("What would you like to do?")
     choice = input().strip().lower()
     if choice == "living room":
         livingRoom()
+    elif choice == "search for items":
+        slowText("You searched the room and found a ")
     else:
         print("Invalid choice. Please try again.")
         time.sleep(2)
         kitchen()
 
-def bedroom(): 
+def kitchen(): 
+    global inv
     os.system('cls' if os.name == 'nt' else 'clear')
-    slowText("You are in the bedroom. There are doors to the living room and garden.")
+    print("backpack: {}".format(backpack))
+    slowText("You are in the kitchen. There are doors to the living room and garden. You can also search for items")
     slowText("What would you like to do?")
     choice = input().strip().lower()
     if choice == "living room":
         bedroom()
     elif choice == "garden":
         garden()
+    elif choice == "search for items":
+        slowText("You searched the room and found a ")
     else:
         print("Invalid choice. Please try again.")
         time.sleep(2)
         bedroom()
 
 def garden(): 
+    global inv
     os.system('cls' if os.name == 'nt' else 'clear')
-    slowText("You are in the garden. There are doors to the bedroom and living room.")
+    print("backpack: {}".format(backpack))
+    slowText("You are in the garden. There are doors to the kitchen and living room. You can also search for items")
     slowText("What would you like to do?")
     choice = input().strip().lower()
-    if choice == "bedroom":
+    if choice == "kitchen":
         bedroom()
     elif choice == "living room":
         livingRoom()
+    elif choice == "search for items":
+        if search(backpack, "shovel"):
+            slowText("You searched the garden and found nothing...")
+            time.sleep(2)
+            garden()
+        else:
+            slowText("You searched the garden and found a shovel, do you want to pick it up?")
+            choice = input().strip().lower()
+            if choice == "yes":
+                backpack.append("shovel")
+                time.sleep(2)
+                garden()
     else:
         print("Invalid choice. Please try again.")
         time.sleep(2)
         garden()
 
+def search(pack, item):
+    found = False
+    for i in range (len(pack)):
+        if pack[i] == item:
+            found = True
+    return found 
 
-
-
+backpack = []
 playerName = ""
 start()
 
