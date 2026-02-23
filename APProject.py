8d000e42abeff1d4d997a1911631886af1912b85
28oswaldohernandezonate-star <28.oswaldo.hernandezonate@carolinaday.org>
2026-02-23 20:25:12 +0000

    AP Project

---
+++ APProject.py
@@ -0,0 +1,31 @@
+import random
+
+def diceRoll():
+ return random.randint (1,6) + random.randint(1,6)
+
+def Avg(num):
+ num = 0 
+ 
+ 
+
+
+avg = []
+L = 0
+
+while L < 100:
+ avg.append(diceRoll())
+ L += 1
+
+for i in range(len(avg)):
+ num7 = 0
+   
+
+
+print(avg)
+
+
+   
+
+
+ 
+
