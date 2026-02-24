07a184485b479a4d6702c24d256353e380e00232
28oswaldohernandezonate-star <28.oswaldo.hernandezonate@carolinaday.org>
2026-02-24 19:51:23 +0000

    done

--- APProject.py
+++ APProject.py
@@ -3,29 +3,26 @@ import random
 def diceRoll():
  return random.randint (1,6) + random.randint(1,6)
 
-def Avg(num):
- num = 0 
- 
- 
+def rollNum():
+ global list    # cycles through every number spits out how many times each number got rolled
+ cycle = 2
+ per = 0
+ while cycle <= 12:
+  non = 0
+  for i in range(len(list)):
+   if list[i] == cycle:
+    non += 1
+    per = (non / len(list)) * 100
+  print ("Out of " + (str(len(list)) + " rolls, " + (str(cycle)) + " rolled " + str(non) + " times or "+ (str(round(per, 2))) + " percent of the time." ))
+  cycle += 1
+   
 
-
-avg = []
+list = []
 L = 0
+numsim = int(input("How many simulations do you want to run?"))
 
-while L < 100:
- avg.append(diceRoll())
+while L < numsim:
+ list.append(diceRoll()) #function called inside loop, input determines amount of sims
  L += 1
 
-for i in range(len(avg)):
- num7 = 0
-   
-
-
-print(avg)
-
-
-   
-
-
- 
-
+print(rollNum())
